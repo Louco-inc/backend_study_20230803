4049f8bf24d172aa405706e7d4bd1d47a37dd47b
mura-work <murata.naoya.work@gmail.com>
2023-08-02 22:12:51 +0900

    first commit

---
+++ src/test/java/com/example/backend_study_20230803/BackendStudy20230803ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_study_20230803;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStudy20230803ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
