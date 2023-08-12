5b1e8e41edbb39ba52e2735d5bd0651cc1679835
MoriShun89 <s.moririn0724@gmail.com>
2023-08-12 13:03:28 +0900

    [feat] 会社名検索API作成

--- src/main/java/com/example/backend_study_20230803/repository/CompanyRepository.java
+++ src/main/java/com/example/backend_study_20230803/repository/CompanyRepository.java
@@ -7,4 +7,7 @@ import com.example.backend_study_20230803.entity.Company;
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
   List<Company> findAll();
 	Company findById(Number id);
+
+  // 会社名で検索をするためのfindByCompanyNameメソッドを作成
+  List<Company> findByCompanyNameLike(String companyNameLike);
 }
