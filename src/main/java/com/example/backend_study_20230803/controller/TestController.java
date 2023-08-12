ae0579608c1b597a619455e60dacfd326f76427b
mura-work <murata.naoya.work@gmail.com>
2023-08-12 11:26:44 +0900

    [feat] 検索フォームと検索結果画面の作成 WIP

--- src/main/java/com/example/backend_study_20230803/controller/TestController.java
+++ src/main/java/com/example/backend_study_20230803/controller/TestController.java
@@ -3,33 +3,45 @@ package com.example.backend_study_20230803.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend_study_20230803.entity.Test;
+import com.example.backend_study_20230803.form.CompanyForm;
+import com.example.backend_study_20230803.service.CompanyService;
 import com.example.backend_study_20230803.service.TestService;
 
-@RestController
-@RequestMapping("/api/v1")
-@CrossOrigin
+@Controller
 public class TestController {
 
+	// @Autowired
+	// TestService service;
+
+	// @RequestMapping(value = "/test_all")
+	// @CrossOrigin
+	// public List<Test> getAllTest() {
+	// List<Test> tests = service.findAll();
+	// return tests;
+	// }
+
+	// @RequestMapping(value = "/test/{id}")
+	// @CrossOrigin
+	// public Test getTest(@PathVariable Number id) {
+	// Test test = service.findById(id);
+	// return test;
+	// }
+
 	@Autowired
-	TestService service;
+	CompanyService service;
 
-	@RequestMapping(value = "/test_all")
-	@CrossOrigin
-	public List<Test> getAllTest() {
-		List<Test> tests = service.findAll();
-		return tests;
-	}
-
-	@RequestMapping(value = "/test/{id}")
-	@CrossOrigin
-	public Test getTest(@PathVariable Number id) {
-		Test test = service.findById(id);
-		return test;
+	@GetMapping("/home")
+	public String home(Model model, @ModelAttribute CompanyForm book) {
+		model.addAttribute("companies", service.findAll());
+		return "companies/index";
 	}
 }
